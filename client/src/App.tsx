b48ccfde4dbee208144119f0e46514b5493f0ad5
Isac Hansson <isac.hansson@ctk.se>
2024-02-27 10:27:44 +0100

    Of course!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,12 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-        crossOrigin="anonymous"
-      />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
