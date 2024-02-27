5acf32cb10e3afcd80851eef710d84db9384d4cc
Isac Hansson <isac.hansson@ctk.se>
2024-02-27 11:12:48 +0100

    Fonts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,6 +9,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap')
+      </style>
     <App />
   </React.StrictMode>
 );
