a5ead1d11640264b664836d583613f1e2ed79ce7
Isac Hansson <isac.hansson@ctk.se>
2024-02-27 10:37:55 +0100

    Init, Home, Header page

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const Header: React.FC = () => {
+
+    return (
+        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
+            
+        </Navbar>
+    );
+}
+
+export default Header;
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import Header from "../components/Header/Header";
+
+
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default HomePage;
