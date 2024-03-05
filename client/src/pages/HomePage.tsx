f2338f1f54398e00c1029e0d9b130f2307ece011
William <william.hp.frisk@gmail.com>
2024-03-05 10:08:14 +0100

    Added simple upload mp4 button

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
+import UploadButton from "../components/uploadbutton/UploadButton";
 
 const HomePage: React.FC = () => {
-  return <Container fluid className="flex-grow-1"></Container>;
+  return (
+    <Container fluid className="flex-grow-1">
+      <Row className="h-100">
+        <Col
+          md={6}
+          className="d-flex align-items-center justify-content-center"
+        >
+          <UploadButton />
+        </Col>
+        <Col md={6}></Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default HomePage;
