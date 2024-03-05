7fa417a7bf580aac92dd35faacae2b6d9c708d7a
Isac Hansson <isachansson1218@gmail.com>
2024-03-05 10:18:54 +0100

    Add about-us page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,16 +6,22 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import HomePage from "./pages/HomePage";
 import { Container } from "react-bootstrap";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AboutUs from "./pages/AboutUs/AboutUs";
 function App() {
   return (
-    <div className="App">
-      <Container fluid className="p-0 d-flex flex-column vh-100">
-        <Header />
-        <HomePage />
-        <Footer />
-      </Container>
-    </div>
+    <Router>
+      <div className="App">
+        <Container fluid className="p-0 d-flex flex-column vh-100">
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about-us" element={<AboutUs />} />
+          </Routes>
+          <Footer />
+        </Container>
+      </div>
+    </Router>
   );
 }
 
