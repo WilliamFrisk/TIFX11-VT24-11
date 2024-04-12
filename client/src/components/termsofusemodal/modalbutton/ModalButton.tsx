38d07a73b7a2440d4474d9cbb193fd2add949578
Isac Hansson <isachansson1218@gmail.com>
2024-04-12 13:18:16 +0200

    Style results and disable upload button

--- client/src/components/termsofusemodal/modalbutton/ModalButton.tsx
+++ client/src/components/termsofusemodal/modalbutton/ModalButton.tsx
@@ -5,12 +5,21 @@ import { Col } from "react-bootstrap";
 interface ModalButtonProps {
   text: string;
   onClick: Function;
+  disabled?: boolean; // Add disabled prop
 }
 
-const ModalButton: React.FC<ModalButtonProps> = ({ text, onClick }) => {
+const ModalButton: React.FC<ModalButtonProps> = ({
+  text,
+  onClick,
+  disabled,
+}) => {
   return (
     <Col md={4} className="d-flex justify-content-center mb-2">
-      <button className={styles.button} onClick={() => onClick()}>
+      <button
+        className={styles.button}
+        onClick={() => onClick()}
+        disabled={disabled}
+      >
         {text}
       </button>
     </Col>
